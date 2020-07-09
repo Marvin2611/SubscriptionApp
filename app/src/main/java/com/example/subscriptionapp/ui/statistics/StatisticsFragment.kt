f9fbd4c917724223a73cf0f153687b54a7d0c99b
Michael Krysmalski <realjupiter@hotmail.de>
2020-07-09 13:26:09 +0200

    Edited Home Fragment

--- app/src/main/java/com/example/subscriptionapp/ui/statistics/StatisticsFragment.kt
+++ app/src/main/java/com/example/subscriptionapp/ui/statistics/StatisticsFragment.kt
@@ -30,22 +30,28 @@ class StatisticsFragment : Fragment() {
 
         val root = inflater.inflate(R.layout.fragment_statistics, container, false)
 
+        val subscriptionAmountValue:TextView=root.findViewById(R.id.SubAmount)
+        val costPerDayValue:TextView=root.findViewById(R.id.costPerDayValue)
+        val costPerWeekValue: TextView = root.findViewById(R.id.costPerWeekValue)
         val costPerMonthValue: TextView = root.findViewById(R.id.costPerMonthValue)
         val costPerYearValue: TextView = root.findViewById(R.id.costPerYearValue)
         val highestValue: TextView = root.findViewById(R.id.mostExpensiveValue)
         val lowestValue: TextView = root.findViewById(R.id.cheapestValue)
         val averageValue: TextView = root.findViewById(R.id.averageValue)
 
-        UpdateLayout(costPerMonthValue, costPerYearValue, highestValue, lowestValue, averageValue)
+        UpdateLayout(subscriptionAmountValue,costPerDayValue,costPerWeekValue,costPerMonthValue, costPerYearValue, highestValue, lowestValue, averageValue)
 
         return root
     }
 
-    fun UpdateLayout(pmV: TextView, pyV: TextView, hV: TextView, lV: TextView, aV: TextView) {
+    fun UpdateLayout(saV:TextView,pdV:TextView,pwV:TextView, pmV: TextView, pyV: TextView, hV: TextView, lV: TextView, aV: TextView) {
+        saV.text=(model.stats.AboCount().toString()+" active Subscriptions!")
+        pdV.text=(model.stats.TotalSum).toString()+"€"
+        pwV.text=(model.stats.TotalSum*7).toString()+"€"
         pmV.text = (model.stats.TotalSum*30).toString()+"€"
         pyV.text = (model.stats.TotalSum*365).toString()+"€"
-        hV.text = (model.stats.highest*30).toString()+"€"
-        lV.text= (model.stats.lowest*30).toString()+"€"
-        aV.text = (model.stats.average*30).toString()+"€"
+        hV.text = (model.stats.highest).toString()+"€"
+        lV.text= (model.stats.lowest).toString()+"€"
+        aV.text = (model.stats.average).toString()+"€"
     }
 }
